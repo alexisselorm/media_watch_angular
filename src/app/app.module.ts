b32ef57ea216b15876d67509781d102f589ee841
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-12-14 07:51:32 +0000

    'Using other components in a component'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MediaItemComponent],
   imports: [BrowserModule],
   bootstrap: [AppComponent],
 })
