335d3ab6963d227a42f4ed81b52f0f6c434bb545
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-12-14 09:04:02 +0000

    'Structural directives: ngFor'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
+import { MediaItemListComponent } from './media-item-list.component';
 
 @NgModule({
-  declarations: [AppComponent, MediaItemComponent],
+  declarations: [AppComponent, MediaItemComponent, MediaItemListComponent],
   imports: [BrowserModule],
   bootstrap: [AppComponent],
 })
