c24db7d62fb66ba7ecfb6aff6f60e562519c565f
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-12-14 15:09:56 +0000

    'Attribute directives:Custom'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
+import { FavoriteDirective } from './favorite.directive';
 
 @NgModule({
-  declarations: [AppComponent, MediaItemComponent, MediaItemListComponent],
+  declarations: [
+    AppComponent,
+    MediaItemComponent,
+    MediaItemListComponent,
+    FavoriteDirective,
+  ],
   imports: [BrowserModule],
   bootstrap: [AppComponent],
 })
