874a570640306cbb579e49514a08a0dccaf86e4f
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-12-15 15:20:03 +0000

    'Working with route params'

--- src/app/media-item-list.component.ts
+++ src/app/media-item-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { MediaItemService } from './media-item.service';
 @Component({
   selector: 'mw-media-item-list',
@@ -7,17 +9,26 @@ import { MediaItemService } from './media-item.service';
 })
 export class MediaItemListComponent implements OnInit {
   mediaItems: any;
-  medium = '';
-  constructor(private mediaItemService: MediaItemService) {}
+  medium: string | null = '';
+  constructor(
+    private mediaItemService: MediaItemService,
+    private activatedRoute: ActivatedRoute
+  ) {}
   ngOnInit(): void {
-    this.getMediaItems(this.medium);
+    this.activatedRoute.paramMap.subscribe((paramMap) => {
+      let medium = paramMap.get('medium');
+      if (medium!.toLocaleLowerCase() === 'all') {
+        medium = '';
+      }
+      this.getMediaItems(medium);
+    });
   }
   onMediaItemDelete(mediaItem: any) {
     this.mediaItemService
       .delete(mediaItem)
       .subscribe(() => this.getMediaItems(this.medium));
   }
-  getMediaItems(medium: string) {
+  getMediaItems(medium: string | null) {
     this.medium = medium;
     this.mediaItemService.get(medium).subscribe((mediaItems) => {
       this.mediaItems = mediaItems;
