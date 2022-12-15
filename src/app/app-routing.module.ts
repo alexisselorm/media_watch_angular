ca5ff049f43e022460d14f6160110cbedcc07f6a
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-12-15 14:34:56 +0000

    'Setting base href and configuring routes'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MediaItemListComponent } from './media-item-list.component';
+import { MediaItemComponent } from './media-item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add', component: MediaItemComponent },
+  { path: ':medium', component: MediaItemListComponent },
+  { path: '', redirectTo: 'all', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
